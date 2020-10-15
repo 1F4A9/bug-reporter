41dca90c77300f9771aa65af5346b987f3e63818
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-10-15 09:31:17 +0200

    admin page

--- admin/main.js
+++ admin/main.js
@@ -1,4 +1,4 @@
-import { data } from '../db/data.js';
+import { data } from '../server/data.js';
 
 generateTable();
 
@@ -13,6 +13,69 @@ function createHyperlink(value) {
   return a;
 }
 
+function createButtons(tr) {
+  const AMOUNT_OF_BUTTONS = 2;
+  const NAME_OF_BUTTONS = ['Edit', 'Delete'];
+
+  for (let i = 0; i < AMOUNT_OF_BUTTONS; i++) {
+    const td = document.createElement('td');
+    const button = document.createElement('button');
+    const textNode = document.createTextNode(NAME_OF_BUTTONS[i]);
+
+    if (NAME_OF_BUTTONS[i] === 'Edit') {
+      button.addEventListener('click', (e) => onEdit(e));
+    } else {
+      button.addEventListener('click', (e) => onDelete(e));
+    }
+
+    button.appendChild(textNode)
+
+    td.appendChild(button);
+    tr.appendChild(td);
+  }
+}
+
+function onEdit(e) {
+  const STATUS_PROPERTIES = ['not started', 'in progress', 'done'];
+
+  const trElement = e.target.parentNode.parentNode;
+  const tdElement = trElement.getElementsByClassName('edit')[0];
+  const currentStatus = tdElement.textContent;
+  tdElement.textContent = '';
+
+  const select = document.createElement('select');
+
+  STATUS_PROPERTIES.forEach(status => {
+    const option = document.createElement('option');
+    const textNode = document.createTextNode(status);
+
+    option.setAttribute('value', status);
+
+    if (status === currentStatus) {
+      option.setAttribute('selected', 'selected');
+    }
+
+    option.appendChild(textNode);
+
+    select.appendChild(option);
+  })
+
+  tdElement.appendChild(select)
+
+  // append the selected option and removes select element
+  select.addEventListener('change', (e) => {
+    const textNode = document.createTextNode(e.target.value);
+
+    select.remove();
+    tdElement.appendChild(textNode);
+  });
+}
+
+function onDelete(e) {
+  const trElement = e.target.parentNode.parentNode;
+  trElement.remove();
+}
+
 function generateTable() {
   const table = document.getElementById('table-tbody');
 
@@ -20,18 +83,28 @@ function generateTable() {
     const tr = document.createElement('tr');
 
     for (let property in obj) {
-      const td = document.createElement('td');
+      // Won´t add ID as a td row.
+      if (property !== '_id') {
+        const td = document.createElement('td');
 
-      const textNode = document.createTextNode(obj[property]);
+        const textNode = document.createTextNode(obj[property]);
 
-      if (property === 'link' || property === 'image') {
-        td.appendChild(createHyperlink(obj[property]));
-      } else {
-        td.appendChild(textNode);
+        if (property === 'status') {
+          td.classList.add('edit');
+        }
+
+        if (property === 'link' || property === 'image') {
+          td.appendChild(createHyperlink(obj[property]));
+        } else {
+          td.appendChild(textNode);
+        }
+
+        tr.appendChild(td);
       }
-
-      tr.appendChild(td);
     }
+
+    createButtons(tr);
+
     table.appendChild(tr);
   }
 }
